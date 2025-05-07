dd5aa49840225d08bbcca9226e3989dac077c55f
Gatwiri Ireri <gatwiriireri@gmail.com>
2025-05-07 06:58:44 +0300

    Revised Week 7 Assignment

--- answers.sql
+++ answers.sql
@@ -1,47 +1,51 @@
 -- Question 1
-SELECT 
-    orderId,
-    customerName,
-    SUBSTRING_INDEX(SUBSTRING_INDEX(products, ',', numbers.n), ',', -1) AS product
-FROM productDetail
-CROSS JOIN (
-    SELECT 1 AS n UNION ALL
-    SELECT 2 UNION ALL
-    SELECT 3 UNION ALL
-    SELECT 4
-) numbers
-WHERE numbers.n <= LENGTH(products) - LENGTH(REPLACE(products, ',', '')) + 1
-ORDER BY orderId, product;
+-- Create a new table 
+CREATE TABLE ProductDetail
+(
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+    PRIMARY KEY (OrderID, Products)
+);
+
+
+
+
+
+
+
 
 
 -- Question 2
 
--- Creating Orders Table
-CREATE TABLE orders (
-    orderId INT PRIMARY KEY AUTO_INCREMENT,
-    customerName VARCHAR(100)
+-- Create the Orders table
+CREATE  TABLE Orders(
+    OrderID INT, PRIMARY KEY,
+    CustomerName VARCHAR(100)
 );
 
--- Populating orders table, which has distinct orderID and customerName
-INSERT INTO orders (orderId, customerName)
-SELECT DISTINCT orderId, customerName
-FROM orderDetails;
-
--- Creating orderItems Table
-CREATE TABLE orderItems (
-    orderId INT AUTO_INCREMENT,
-    product VARCHAR(100),
-    quantity INT,
-    PRIMARY KEY (orderId, product),
-    FOREIGN KEY (orderId) REFERENCES orders(orderId)
+-- Create the OrderDetails
+CREATE TABLE OrderDetails(
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
 
--- populating orderItems table
+-- Insert data into Orders table
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
 
-INSERT INTO orderItems (orderId, product, quantity)
-SELECT orderId, product, quantity
-FROM orderDetails;
-
-
-
-
+-- Insert data into OrderDetails table
+INSERT INTO OrderDetails (OrderID, Product, Quantity)
+VALUES 
+    (101, 'Laptop', 2),
+    (101, 'Mouse', 1),
+    (102, 'Tablet', 3),
+    (102, 'Keyboard', 1),
+    (102, 'Mouse', 2),
+    (103, 'Phone', 1);
